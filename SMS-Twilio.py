305b7191babc07f09b8767f2c34c29e5a5909b5f
Johan-Cuevas <44459215+Johan-Cuevas@users.noreply.github.com>
2019-01-02 21:03:07 -0800

    Add files via upload

---
+++ SMS-Twilio.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client # imports client from twilio
+
+
+account_sid = 'ACxxxxxxxxxxxxx' # account tokens
+auth_token = 'xxxxxxxxxxxxxxxx'
+client = Client(account_sid, auth_token)
+
+a = ['+1xxxxxxxx', '++1xxxxxxxx'] # list of numbers to send message to
+
+for i in a:
+    print(i)
+    client.messages.create(from_= 'Twilio number',
+                                 body = "Message goes here",
+                                 to = i)
