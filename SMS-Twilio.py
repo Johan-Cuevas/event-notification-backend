8841ec7496eb8f71ffd8204eb40a1eada82a7f13
Johan-Cuevas <44459215+Johan-Cuevas@users.noreply.github.com>
2019-01-02 21:03:30 -0800

    Update SMS-Twilio.py

--- SMS-Twilio.py
+++ SMS-Twilio.py
@@ -5,7 +5,7 @@ account_sid = 'ACxxxxxxxxxxxxx' # account tokens
 auth_token = 'xxxxxxxxxxxxxxxx'
 client = Client(account_sid, auth_token)
 
-a = ['+1xxxxxxxx', '++1xxxxxxxx'] # list of numbers to send message to
+a = ['+1xxxxxxxx', '+1xxxxxxxx'] # list of numbers to send message to
 
 for i in a:
     print(i)
