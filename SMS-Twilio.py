a33ba565d82cebbcca74b466369f7e924be59df2
Johan-Cuevas <44459215+Johan-Cuevas@users.noreply.github.com>
2020-02-21 21:31:27 -0800

    Update SMS-Twilio.py

--- SMS-Twilio.py
+++ SMS-Twilio.py
@@ -5,10 +5,9 @@ account_sid = 'ACxxxxxxxxxxxxx' # account tokens
 auth_token = 'xxxxxxxxxxxxxxxx'
 client = Client(account_sid, auth_token)
 
-a = ['+1xxxxxxxx', '+1xxxxxxxx'] # list of numbers to send message to
+phone_numbers = ['+1xxxxxxxx', '+1xxxxxxxx'] # list of numbers to send message to
 
-for i in a:
-    print(i)
+for numbers in phone_numbers:
     client.messages.create(from_= 'Twilio number',
                                  body = "Message goes here",
-                                 to = i)
+                                 to = numbers)
